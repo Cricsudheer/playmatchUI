85aed5f85e08ac0ed4b89e9fd4b849548a39a0fb
sudranjan <sudranjan@ebay.com>
2026-01-10 11:32:40 +0530

    Add configuration files and implement navigation bar styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { LoginPage } from './pages/LoginPage';
 import { SignupPage } from './pages/SignupPage';
 import { ProtectedRoute } from './components/ProtectedRoute';
 import { AuthProvider, useAuth } from './hooks/useAuth.jsx';
+import { Toaster } from 'sonner';
 import './styles/main.css';
 
 /**
@@ -93,6 +94,7 @@ export default function App() {
   return (
     <AuthProvider>
       <Router>
+        <Toaster position="top-right" richColors />
         <AppContent />
       </Router>
     </AuthProvider>
